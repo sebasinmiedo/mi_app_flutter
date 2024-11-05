64ceb739769401fa42c101b2c905aea4f06123f8
sebasinmiedo <sebastianalbertomaqueraochoa@gmail.com>
2024-11-05 11:01:36 -0500

    Segundo commit - Proyecto Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Has presionado el botón estas veces:',
             ),
             Text(
               '$_counter',
